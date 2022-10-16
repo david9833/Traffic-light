f9499cabec146a7c4973442ef765c420013b9a85
david9833 <dagalisteo@gmail.com>
2022-10-16 16:48:34 +0000

    traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+	useEffect(() =>{
+		handleClick
+	}, [color,morado]) 
+
+	const [color, setColor] = useState();
+
+	const [morado, setMorado] =useState(false);
+
+	var addPurple = () => {
+		setMorado(!morado)
+	}
+
+	var handleClick = () => {
+		var listOfColors= ["red", "orange", "green"];
+		var colorIndex = Math.round(Math.random()*2);
+     	setColor(listOfColors[colorIndex])
+	}
+
+
+		return (
+			<>
+			<div className="stick"></div>
+
+			<div className="container d-flex flex-column align-items-center traffic-light">
+				
+				<div onClick={()=>setColor("red")} className={`red` +((color==="red") ? " lightRed" : "")}></div>
+				<div onClick={()=>setColor("orange")} className={`orange`+((color==="orange") ? " lightOrange" : "")}></div>
+				<div onClick={()=>setColor("green")} className={`green`+((color=="green") ? " lightGreen" : "")}></div>
+
+				<div className={((morado==true) ? "purple lightPurple" : "")}></div>
+
+			</div>
+			<div className="container d-flex flex-column">
+				<button onClick={handleClick} className="change"> Random Mode</button>
+				<button onClick={addPurple} className="addPurple">Add Purple</button>
+			</div>
+		
+			</>
+		);
 };
 
 export default Home;
+
